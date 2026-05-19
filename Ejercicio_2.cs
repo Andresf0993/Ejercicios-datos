c7a1f3d6b6539b57b8c48765fc95fcfa9ffa2b6b
Andresf0993 <andres_09_93@hotmail.com>
2026-05-19 10:59:36 -0500

    Create Ejercicio_2.cs

---
+++ Ejercicio_2.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Ejercicio_2
+{
+    public static void Main(string[] args)
+    {
+        double myDouble = 5.45;
+        int myInt = (int) myDouble;
+        Console.WriteLine (myDouble);
+        Console.WriteLine (myInt);
+    }
