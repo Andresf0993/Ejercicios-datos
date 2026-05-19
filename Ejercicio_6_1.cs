1d883298ee2763c6b91fccebc819c8d9c012312b
Andresf0993 <andres_09_93@hotmail.com>
2026-05-19 11:38:01 -0500

    Create Ejercicio_6_1.cs

---
+++ Ejercicio_6_1.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Ejercicio_3
+{
+    public static void Main(string[] args)
+    {
+         int x = 5;
+         int y = 3;
+         
+          Console.WriteLine (x>y
+       
+         );
+    
+         
+        
+        
+    }
+}
