f81c6b18bf08a5bffd247d39d0fa1e6c445b7262
Andresf0993 <andres_09_93@hotmail.com>
2026-05-19 11:29:31 -0500

    Create Ejercicio_4_1.cs

---
+++ Ejercicio_4_1.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Ejercicio_3
+{
+    public static void Main(string[] args)
+    {
+         int sum1 = 100+50;
+         double sum2 = sum1 + 531.74;
+         double sum3 = sum1 + sum2;
+         
+         
+          Console.WriteLine (sum1);
+          Console.WriteLine (sum2);
+          Console.WriteLine (sum3);
+         
+         
+        
+        
+    }
+}
